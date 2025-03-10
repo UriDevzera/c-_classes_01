29d94f2c62938008e5acb09527ee710a950f5123
Uriel <urielgols@hotmail.com>
2025-03-10 00:38:48 -0300

    add - aula 3 operadores e procedencias

---
+++ 3_operadores_procedencia/main.cpp
@@ -0,0 +1,31 @@
+// import biblioteca de entrada e saída
+#include <iostream>
+
+// usando o namespace std
+using namespace std;
+
+int main()
+{
+    int x{3}, y{6};
+
+    cout << "x = " << x << " e y = " << y << '\n';
+    cout << "soma x + y = " << x + y << '\n';
+    cout << "subtrai x - y = " << x - y << '\n';
+    cout << "multiplica x * y = " << x * y << '\n';
+    cout << "divide x / y = " << x / y << '\n';
+    cout << "resto x % y = " << x % y << '\n';
+
+    return 0;
+}
+
+/*
+    Operadores aritméticos
+    int x{3}, y{6};
+
+    cout << "x = " << x << " e y = " << y << '\n';
+    cout << "soma x + y = " << x + y << '\n';
+    cout << "subtrai x - y = " << x - y << '\n';
+    cout << "multiplica x * y = " << x * y << '\n';
+    cout << "divide x / y = " << x / y << '\n';
+    cout << "resto x % y = " << x % y << '\n';
+*/
