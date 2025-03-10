9aef2aae9acdd759be08be65e4b84e72c04e72dd
Uriel <urielgols@hotmail.com>
2025-03-10 00:38:10 -0300

    add - aula 1 primeiro programa

---
+++ 1_primeira_aula_main_cpp/main.cpp
@@ -0,0 +1,16 @@
+// import biblioteca de entrada e saída
+#include <iostream>
+
+// usando o namespace std
+using namespace std;
+
+int main()
+{
+    // imprimindo na tela
+    cout << "Meu primeiro programa em C++ com comentarios" << '\n';
+
+    // sem o using namespace std deve-se usar assim
+    // std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
